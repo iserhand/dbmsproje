cd52d2368e4e94b9f73fc1587e8fde9256d6f5af
Serhan <iserhan2001@gmail.com>
2021-12-28 07:38:49 +0300

    first commit

---
+++ WebSite1/Register.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            &nbsp;
+        </p>
+        <p>
+            &nbsp;
+        </p>
+        <p>
+            <asp:TextBox ID="txtMail" runat="server" Style="margin-left: 92px" Width="360px"></asp:TextBox>
+        </p>
+        <asp:TextBox ID="txtPassword" runat="server" Style="margin-left: 92px" Width="360px" TextMode="Password" ></asp:TextBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" Style="margin-left: 237px" Text="Register" OnClick="Button1_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Register.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (txtPassword.Text.Length < 6|| txtPassword.Text.Length >18)
+        {   
+            
+            //Password contains less than 6 characters or more than 18 characters.
+            txtPassword.BorderColor = System.Drawing.Color.Red;
+        }
+        else
+        {
+            
+            if (txtPassword.Text.Any(ch => !Char.IsLetterOrDigit(ch)||Char.IsWhiteSpace(ch)))
+            {
+                //Password has special characters 
+                txtPassword.BorderColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                txtPassword.BorderColor = System.Drawing.Color.Empty;
+                //Go register
+            }
+        }
+
+    }
+}
