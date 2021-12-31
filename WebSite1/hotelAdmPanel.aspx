a6906e12e2f130eb555b8e2b0e8ea7d23ab78230
Serhan <iserhan2001@gmail.com>
2021-12-31 16:31:45 +0300

    fileupload

---
+++ WebSite1/hotelAdmPanel.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="hotelAdmPanel.aspx.cs" Inherits="hotelAdmPanel" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 406px">
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            
+            <br />
+            <asp:Button ID="btnSaveImg" runat="server" Text="Add Image" OnClick="btnSaveImg_Click" />
+            
+            <br />
+            <asp:Label ID="lblmessage" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/hotelAdmPanel.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class hotelAdmPanel : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSaveImg_Click(object sender, EventArgs e)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        if (FileUpload1.HasFile)
+        {
+            try
+            {
+                sb.AppendFormat(" Uploading file: {0}", FileUpload1.FileName);
+
+                //saving the file
+                string strFileName;
+                string strFilePath;
+                string strFolder;
+                strFolder = Server.MapPath("./");
+
+                strFileName = FileUpload1.PostedFile.FileName;
+                strFileName = Path.GetFileName(strFileName);
+                if (!Directory.Exists(strFolder))
+                {
+                    Directory.CreateDirectory(strFolder);
+                }
+                strFilePath = strFolder + strFileName;
+                
+
+
+                FileUpload1.PostedFile.SaveAs(strFilePath);
+
+                //Showing the file information
+                sb.AppendFormat("<br/> Save As: {0}", FileUpload1.PostedFile.FileName);
+                sb.AppendFormat("<br/> File type: {0}", FileUpload1.PostedFile.ContentType);
+                sb.AppendFormat("<br/> File length: {0}", FileUpload1.PostedFile.ContentLength);
+                sb.AppendFormat("<br/> File name: {0}", FileUpload1.PostedFile.FileName);
+
+            }
+            catch (Exception ex)
+            {
+                sb.Append("<br/> Error <br/>");
+                sb.AppendFormat("Unable to save file <br/> {0}", ex.Message);
+            }
+        }
+        else
+        {
+            lblmessage.Text = sb.ToString();
+        }
+        lblmessage.Text = sb.ToString();
+    }
+}
