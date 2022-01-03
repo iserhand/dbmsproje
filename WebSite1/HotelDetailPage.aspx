bc5e9d3610dc55d9435a16c8dd2a18dce2c5e46f
bilgehanerten <bilgehanerten@gmail.com>
2022-01-03 19:29:07 +0300

    // hotel detail page

---
+++ WebSite1/HotelDetailPage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="HotelDetailPage.aspx.cs" Inherits="HotelDetailPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 634px; width: 1468px">
+            <asp:Image ID="hotelImg" runat="server" Height="109px" Width="133px" />
+            <asp:TextBox ID="hotelDetails" runat="server" Height="99px" style="margin-right: 0px" Width="322px" TextMode="MultiLine"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Make Reservation" />
+            <asp:TextBox ID="commentBox" runat="server" Height="100px" TextMode="MultiLine" Width="340px"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/HotelDetailPage.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HotelDetailPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Session.Add("hotelID", 1);
+        show_details();
+    }
+
+
+    public void show_details()
+    {
+        Dbhelper helper = new Dbhelper();
+
+        Object hotelID = Session["hotelID"];
+        using (SqlCommand cmd = new SqlCommand("Select  * FROM [hotels] WHERE ID = " + Convert.ToInt32(hotelID), helper.connect()))
+        {
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                String hotelName = (reader.GetString(1));
+                String hotelLocation = (reader.GetString(2));
+                String roomCount = (reader.GetString(3));
+                String hotelRating = (reader.GetString(4));
+                String ratingStar = (reader.GetString(5));
+
+                hotelDetails.Text = hotelName + " " + hotelLocation + " " + hotelName + " " + roomCount + " " + hotelRating + " " + ratingStar;
+
+                reader.Close();
+            }
+        }
+
+        using (SqlCommand cmd2 = new SqlCommand("Select  * FROM [comment] WHERE id = " + Convert.ToInt32(hotelID), helper.connect()))
+        {
+            using (SqlDataReader reader2 = cmd2.ExecuteReader())
+            {
+                String id = (reader2.GetString(1));
+                String userID = (reader2.GetString(2));
+                String comment = (reader2.GetString(3));
+                String hotelId = (reader2.GetString(4));
+
+                commentBox.Text = comment;
+
+                reader2.Close();
+            }
+        }
+
+        helper.close();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
